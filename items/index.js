c0fb03f745e4b03a784d4f8f6ebf2e0ac3f9f2d8
Dean Crossman <deancro@Deans-MacBook-Pro.local>
2020-12-19 15:59:06 -0500

    face insert

--- items/index.js
+++ items/index.js
@@ -1,4 +1,4 @@
-export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/FavouriteFoodBaskets750x500_xraj9x.png"},
+export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/Personal_Face.png"},
 {"id": "item2", "fname":"item2.md", "specialImage":"/img/GMP1.png"},
 {"id": "item3", "fname":"item3.md", "specialImage":"/img/GMP2.png"},
 {"id": "item4", "fname":"item4.md", "specialImage":"https://res.cloudinary.com/salesucation-com-inc/image/upload/v1523215138/DroidfootGame750x500_bwphbe.png"},
